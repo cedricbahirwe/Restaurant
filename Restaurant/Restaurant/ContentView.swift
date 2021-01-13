328367da3e231cbe6d741e5356bfd36298cc2a7d
cedricbahirwe <cedricaganzebahirwe@gmail.com>
2021-01-13 17:19:45 +0200

    Add Initial updated porject version

---
+++ Restaurant/Restaurant/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Restaurant
+//
+//  Created by Cédric Bahirwe on 13/01/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
