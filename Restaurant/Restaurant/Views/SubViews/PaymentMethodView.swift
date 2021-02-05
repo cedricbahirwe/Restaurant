68bc2a03f777d06c9ae000fb1ee1d885beda987b
cedricbahirwe <cedricaganzebahirwe@gmail.com>
2021-02-05 10:14:59 +0200

    Add Payment Method view

---
+++ Restaurant/Restaurant/Views/SubViews/PaymentMethodView.swift
@@ -0,0 +1,20 @@
+//
+//  PaymentMethodView.swift
+//  Restaurant
+//
+//  Created by Cédric Bahirwe on 05/02/2021.
+//
+
+import SwiftUI
+
+struct PaymentMethodView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PaymentMethodView_Previews: PreviewProvider {
+    static var previews: some View {
+        PaymentMethodView()
+    }
+}
