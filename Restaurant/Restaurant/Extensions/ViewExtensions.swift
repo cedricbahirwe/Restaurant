d26e391db9e7c3dcd4f21bd9f8100fb167bfc281
cedricbahirwe <cedricaganzebahirwe@gmail.com>
2021-02-16 00:00:59 +0200

    Keyboard Manager update class

--- Restaurant/Restaurant/Extensions/ViewExtensions.swift
+++ Restaurant/Restaurant/Extensions/ViewExtensions.swift
@@ -22,6 +22,18 @@ extension View {
     }
 }
 
+
+struct RoundedCorner: Shape {
+    
+    var radius: CGFloat = .infinity
+    var corners: UIRectCorner = .allCorners
+    
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        return Path(path.cgPath)
+    }
+}
+
 struct Dot: View {
     var body: some View {
         Text("·").bold().scaleEffect(1.5)
--- Restaurant/Restaurant/Helpers/Helpers.swift
+++ Restaurant/Restaurant/Helpers/Helpers.swift
@@ -10,17 +10,6 @@ import SwiftUI
 import Combine
 
 
-struct RoundedCorner: Shape {
-    
-    var radius: CGFloat = .infinity
-    var corners: UIRectCorner = .allCorners
-    
-    func path(in rect: CGRect) -> Path {
-        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
-        return Path(path.cgPath)
-    }
-}
-
 struct SheetView<Content: View> : View {
     
     @Binding var showCard: Bool
